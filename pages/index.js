7d084b18e8401ec2bead226da4b742aa96d174a6
Gean Santos <105807310+geansdev@users.noreply.github.com>
2025-12-19 15:58:05 +0000

    adicionado pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Como foi seu dia?</h1>
+    return <h1>Kimberly Perreira Ribas, amo você!
+                Se me ama, dá uma risadinha…
+                Se não, dá um mortal para trás
+            </h1>
 }
 
 export default Home;
